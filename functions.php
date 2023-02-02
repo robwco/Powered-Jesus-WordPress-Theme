674f9a7dca9a5471460753440a8d50db1dde27ca
Robert Williams <890711+robwco@users.noreply.github.com>
2023-02-02 11:03:10 -0800

    Added ACF support

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
-
+/************************* 
+Register Sermon Post Type
+*************************/
 register_post_type(
     'sermon',
     array(
@@ -23,3 +25,68 @@ register_post_type(
         'supports'  => array( 'title', 'editor', 'revisions', 'author', 'excerpt', 'page-attributes', 'thumbnail', 'custom-fields', 'post-formats' ),
     )
 );
+/************************* 
+Require and Install ACF Plugin
+*************************/
+// Define path and URL to the ACF plugin.
+define( 'MY_ACF_PATH', get_stylesheet_directory() . '/includes/acf/' );
+define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/includes/acf/' );
+// Include the ACF plugin.
+include_once( MY_ACF_PATH . 'acf.php' );
+// Customize the url setting to fix incorrect asset URLs.
+add_filter('acf/settings/url', 'my_acf_settings_url');
+function my_acf_settings_url( $url ) {
+    return MY_ACF_URL;
+}
+// (Optional) Hide the ACF admin menu item.
+add_filter('acf/settings/show_admin', '__return_false');
+/************************* 
+Create Sermon Custom Fields
+*************************/
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array (
+    'key' => 'sermon_group',
+    'title' => 'Sermon Fields',
+    'fields' => array (
+        array (
+            'key' => 'field_sermons_1',
+            'label' => 'Sermon Video',
+            'name' => 'sermon_video',
+            'type' => 'text',
+            'prefix' => '',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array (
+                'width' => '30%',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => '',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+            'readonly' => 0,
+            'disabled' => 0,
+        )
+    ),
+    'location' => array (
+        array (
+            array (
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'sermon',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+));
+
+endif;
--- header.php
+++ header.php
@@ -8,8 +8,9 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inconsolata&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Albert+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">  
     <?php wp_head(); ?>
 </head>
 
